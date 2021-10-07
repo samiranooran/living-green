022e9839ff4601315f214348e7ae341b31296cb8
saee0043 <saee0043@algonquinlive.com>
2021-10-07 12:30:17 -0400

    Register added

--- main.py
+++ main.py
@@ -1,1 +1,63 @@
 # This will be our main project file, all our Python code will be in this file (Routes, MySQL connection, validation, etc).
+
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+import os
+from dotenv import load_dotenv
+from flask_bcrypt import Bcrypt
+
+load_dotenv()
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+# Change this to your secret key (can be anything, it's for extra protection)
+app.secret_key = 'abcd'
+
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+
+# Intialize MySQL
+mysql = MySQL(app)
+
+
+# http://localhost:5000/register - this will be the registration page, we need to use both GET and POST requests
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)',
+                           (username, bcrypt.generate_password_hash(password).decode('utf-8'), email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('en/register.html', msg=msg)
---
+++ run.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("set FLASK_APP=main.py & set FLASK_DEBUG=1 & flask run")
