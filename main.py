bfa6a0f2edb49b479198e70d82985520cc886ecc
AARSH0010 <dosh0010@algonquinlive.com>
2021-10-07 19:30:13 -0400

    Login Functionality Added

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ mysql = MySQL(app)
 
 
 # http://localhost:5000/register - this will be the registration page, we need to use both GET and POST requests
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register/', methods=['GET', 'POST'])
 def register():
     # Output message if something goes wrong...
     msg = ''
@@ -61,3 +61,34 @@ def register():
         msg = 'Please fill out the form!'
     # Show registration form with message (if any)
     return render_template('en/register.html', msg=msg)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    #if you're logged in you should not be able to see the login page
+    if 'loggedin' in session:
+        return 'You are already logged in'
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        result = cursor.execute('SELECT * FROM accounts WHERE username = %s', [username])
+        # Fetch one record and return result
+        if result > 0:
+            account = cursor.fetchone()
+            password1 = account['password']
+            if bcrypt.check_password_hash(password1, password):
+                session['loggedin'] = True
+                session['id'] = account['id']
+                session['username'] = account['username']
+                return 'You have successfully logged in'
+            else:
+                msg = 'Incorrect password!'
+        else:
+            msg = 'Incorrect Username!'
+    return render_template('en/login.html', msg=msg)
