ef24276be174d8e5514b733b61ff9df7101585ce
Pardis Feizi <feiz0009@algonquinlive.com>
2021-10-08 11:20:03 -0400

    add home and profile page

--- main.py
+++ main.py
@@ -61,3 +61,26 @@ def register():
         msg = 'Please fill out the form!'
     # Show registration form with message (if any)
     return render_template('en/register.html', msg=msg)
+
+
+@app.route('/pythonlogin/home')
+def home():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+@app.route('/pythonlogin/profile')
+def profile():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
